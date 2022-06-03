4c8c43f1808e2f47e4f595b54b69ec4b50411416
MisaelCartes <mis.cartes@duocuc.cl>
2022-06-03 16:09:53 -0400

    validacion registro

--- JS/script.js
+++ JS/script.js
@@ -14,4 +14,37 @@ function validarlogin()
     }
 }
 
+function validarRegistro()
+{
+    var nombre=document.getElementById("nombre").value;
+    var correo=document.getElementById("correo").value;
+    var contra=document.getElementById("pass2").value;
+    var contra2=document.getElementById("pass3").value;
+    if( nombre && correo && contra && contra2)
+    {
+        if(nombre.length>=4)
+        {
+            if(correo.includes("@gmail")||correo.includes("@hotmail")||correo.includes("@outlook"))
+            {
+                if(contra.length>=6)
+                {
+                    if(contra==contra2)
+                    {
+                        alert("Registrado correctamente");
+                        window.location="./Paginalogin.html"
+     
+
+                    }
+                    else{alert("Las contraseñas no son iguales.")}
+                }
+                else{alert("Contraseña debe tener almenos 6 caracteres");}
+            }
+            else{alert("Correo no contiene dominio");}
+        }
+        else {alert("El nombre debe tener almenos 4 caracteres");}
+
+    }
+    else{ alert("Datos invalidos")}
+}
+
 
