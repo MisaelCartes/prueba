e843767f084a5f5e5509e973bbde7b61e09e24fc
MisaelCartes <mis.cartes@duocuc.cl>
2022-06-03 10:32:13 -0400

    Validador inicio sesion agregado hacer lo mismo con registro

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,17 @@
+function validarlogin()
+{
+    var usuario=document.getElementById("user").value;
+    var password=document.getElementById("pass").value;
 
+    if(usuario=="user1" && password=="user1pass")
+    {
+        window.location="./index.html"
+        //CAMBIAR AL PERFIL USUARIO 
+    }
+    else
+    {
+        alert("Datos invalidos")
+    }
+}
 
 
